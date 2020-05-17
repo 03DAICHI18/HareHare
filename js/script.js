4b470a6f1888ed5d54a22aae2599376a549c16dc
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-05-17 13:56:34 +0900

    オープニングをローディング画面化、ページ内スムーススクロールの実装

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+  // ・・・・・・・・・・・・・・・・・・ページ内スクロール・・・・・・・・・・・・・・・・・・
+  $('a[href^="#"]').click(function (event) {
+    // クリックした要素のhref属性の値を取得
+    var anchor = $(this).attr("href");
+    // 表示位置を取得
+    var position = $(anchor).offset().top;
+    // アニメーションの効果の作成
+    $('body,html').animate({ scrollTop: position }, 500);
+    return false;
+  });
+});
